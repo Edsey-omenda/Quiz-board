0127c12ae148439f9f49c153853887048c8d4bc0
Edsey omenda <omendajames0317@gmail.com>
2022-05-15 22:29:02 +0300

    links  the  html  to  the  java  script file

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ alert("Welcome  to Java script  quizes");
 function  confirm()  {
     document.getElementById("after_start").style.visibility="visible";
 }
-const buttonsubmit = document.getElementById("s-btn");
-
+    
 function search() {
     var testOne = document.getElementById("1");
     var testTwo = document.getElementById("2");
@@ -19,7 +18,7 @@ function search() {
     if (testTwo == "Record output of java programs"){
         score +=1;
     }
-    if (testThree == "Roundup a value  to  the  nearest interger"){
+    if (testThree == "Round up a value  to  the  nearest interger"){
         score +=1;
     }
     if (testFour == "Joining two  or  more  things  together"){
